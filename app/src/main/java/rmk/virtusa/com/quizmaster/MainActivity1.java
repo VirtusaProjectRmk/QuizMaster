72b3405139ce852475d79a1bb58b9bf62190b507
Harisha12 <31131848+Harisha12@users.noreply.github.com>
2018-07-06 21:26:41 +0530

    Update MainActivity1.java

--- app/src/main/java/rmk/virtusa/com/quizmaster/MainActivity1.java
+++ app/src/main/java/rmk/virtusa/com/quizmaster/MainActivity1.java
@@ -6,22 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Calendar;
+
 public class MainActivity1 extends AppCompatActivity {
+
+    Button startQuizButton;
+
+    Calendar calendar = Calendar.getInstance();
+    int day = calendar.get(Calendar.DAY_OF_WEEK);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
-        Button startQuizButton=findViewById(R.id.start_quiz);
+        startQuizButton=findViewById(R.id.start_quiz);
         startQuizButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity1.this,QuizActivity.class);
-                startActivity(intent);
+				if(day == Calendar.SATURDAY || day==Calendar.SUNDAY) {
+                    startActivity(new Intent(MainActivity1.this,NoTestActivity.class));
+
+                } else {
+					Intent intent = new Intent(MainActivity1.this, QuizActivity.class);
+					startActivity(intent);
+                }
             }
         });
-
     }
 
 }
