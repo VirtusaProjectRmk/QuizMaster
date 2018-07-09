10d0094a97ef747238815989313b8f69231cd732
Bharatvaj H <bharatvajph@gmail.com>
2018-07-09 22:07:12 +0530

    Refactored resource Wallpaper to wallpaper and restructured User POJO

--- app/src/main/java/rmk/virtusa/com/quizmaster/model/User.java
+++ app/src/main/java/rmk/virtusa/com/quizmaster/model/User.java
@@ -6,51 +6,25 @@ import com.google.gson.annotations.SerializedName;
 
 public class User {
 
-    @SerializedName("name")
-    @Expose
-    private String name;
 
-    @SerializedName("id")
-    @Expose
     private String id;
 
-
-    @SerializedName("qansTot")
-    @Expose
     private int qAnsTot;
 
-    @SerializedName("qattTot")
-    @Expose
     private int aAttTot;
 
-    @SerializedName("ponitsTot")
-    @Expose
     private int pointsTot;
     
     public User(){
 
     }
 
-    public User(int aAttTot, int qAnsTot, int pointsTot, String id, String name) {
+    public User(int aAttTot, int qAnsTot, int pointsTot, String id) {
         this.qAnsTot = qAnsTot;
         this.aAttTot = aAttTot;
         this.id = id;
         this.pointsTot = pointsTot;
-        this.name = name;
     }
-    
-
-    
-    public String getName() {
-        return this.name;
-    }
-    
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    
     
     public String getId() {
         return this.id;
