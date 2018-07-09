f6dad80be9e10a7dc11b6ce4f77b9e151471cefb
Bharatvaj H <bharatvajph@gmail.com>
2018-07-10 01:59:50 +0530

    Updated UI in ProfileActivity

--- app/src/main/java/rmk/virtusa/com/quizmaster/ProfileActivity.java
+++ app/src/main/java/rmk/virtusa/com/quizmaster/ProfileActivity.java
@@ -1,14 +1,21 @@
 package rmk.virtusa.com.quizmaster;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,36 +24,51 @@ import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.TimeZone;
 
-import rmk.virtusa.com.quizmaster.adapter.StatisticsAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import rmk.virtusa.com.quizmaster.handler.ResourceHandler;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView t;
-    RecyclerView recyclerView;
-    TextView t1;
+    @BindView(R.id.attTV)
+    TextView attTV;
+    @BindView(R.id.poiTV)
+    TextView poiTV;
+    @BindView(R.id.ansTV)
+    TextView ansTV;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    private ProfileActivity profileActivity = this;
 
     ResourceHandler resHandler;
 
     ArrayList<String> stats = new ArrayList<>();
+    @BindView(R.id.profileToolbar)
+    Toolbar profileToolbar;
+    @BindView(R.id.profileImage)
+    CircleImageView profileImage;
+    @BindView(R.id.name)
+    EditText name;
+    @BindView(R.id.useremail)
+    TextView useremail;
+    @BindView(R.id.profileToolbarLayout)
+    CollapsingToolbarLayout profileToolbarLayout;
 
-    void initialize(){
-        t = findViewById(R.id.user);
-        t1 = findViewById(R.id.useremail);
-        recyclerView = findViewById(R.id.userStatisticsRecyclerView);
-    }
-
-    void addStat(String title, String val){
-        stats.add(title + " : " + val);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
 
-        initialize();
+        setSupportActionBar(profileToolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         resHandler = ResourceHandler.getInstance();
 
@@ -55,25 +77,24 @@ public class ProfileActivity extends AppCompatActivity {
             for (UserInfo profile : user.getProviderData()) {
                 // Name
                 String name = profile.getDisplayName();
-                t.setText(name);
+                this.name.setText(name);
                 String email = user.getEmail();
-                t1.setText(email);
+                this.useremail.setText(email);
 
             }
-            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-            recyclerView.setAdapter(new StatisticsAdapter(this, stats));
-            if(resHandler.getUser() != null){
-                addStat(getString(R.string.questions_total_attended), String.valueOf(resHandler.getUser().getAAttTot()));
-                addStat(getString(R.string.questions_total_answered), String.valueOf(resHandler.getUser().getQAnsTot()));
-                addStat(getString(R.string.total_points), String.valueOf(resHandler.getUser().getPointsTot()));
+
+            if (resHandler.getUser() != null) {
+                ansTV.setText(String.valueOf(resHandler.getUser().getQAnsTot()));
+                attTV.setText(String.valueOf(resHandler.getUser().getAAttTot()));
+                poiTV.setText(String.valueOf(resHandler.getUser().getPointsTot()));
             }
         }
     }
 
 
     public void update(View view) {
-       final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-       EditText t1= findViewById(R.id.name);
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        EditText t1 = findViewById(R.id.name);
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                 .setDisplayName(t1.getText().toString().trim())
                 .build();
@@ -87,11 +108,37 @@ public class ProfileActivity extends AppCompatActivity {
                             for (UserInfo profile : user.getProviderData()) {
                                 // Name
                                 String name = profile.getDisplayName();
-                                TextView t= findViewById(R.id.user);
-                                t.setText(name);
+                                profileActivity.name.setText(name);
                             }
                         }
                     }
                 });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.profileLogout) {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(this, "User Logged out", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, LoginActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
