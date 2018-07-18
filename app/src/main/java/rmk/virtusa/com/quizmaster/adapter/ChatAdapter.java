6f082bec0b566b29119fd3cddadd32b0d0b112a4
Bharatvaj H <bharatvajph@gmail.com>
2018-07-19 01:42:26 +0530

    Added basic implementation of ChatActivity ChatHandler and InboxHandler

---
+++ app/src/main/java/rmk/virtusa/com/quizmaster/adapter/ChatAdapter.java
@@ -0,0 +1,68 @@
+package rmk.virtusa.com.quizmaster.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import rmk.virtusa.com.quizmaster.R;
+import rmk.virtusa.com.quizmaster.model.Chat;
+import rmk.virtusa.com.quizmaster.model.Inbox;
+import rmk.virtusa.com.quizmaster.model.User;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+    @BindView(R.id.chatContentTextView)
+    TextView chatContentTextView;
+    @BindView(R.id.chatHeaderTextView)
+    TextView chatHeaderTextView;
+    @BindView(R.id.chatSentTimeTextView)
+    TextView chatSentTimeTextView;
+
+    private Context context;
+    private List<Chat> chats;
+    private Inbox inbox;
+    private List<User> members;
+
+    public ChatAdapter(Context context, List<Chat> chats, Inbox inbox, List<User> members) {
+        this.context = context;
+        this.chats = chats;
+        this.inbox = inbox;
+        this.members = members;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.chat_item, parent, false);
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        Chat chat = chats.get(position);
+        holder.chatContentTextView.setText(chat.getChat());
+    }
+
+    @Override
+    public int getItemCount() {
+        return chats.size();
+    }
+
+    public class ChatViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.chatContentTextView)
+        TextView chatContentTextView;
+
+        public ChatViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+}
