e62e52abac980870e7d987559889e4f3466a1aae
Blessy Prarthana S <blessyprarthanasamuel@gmail.com>
2018-08-18 19:25:20 +0530

    Update DashActivity.java

--- app/src/main/java/rmk/virtusa/com/quizmaster/DashActivity.java
+++ app/src/main/java/rmk/virtusa/com/quizmaster/DashActivity.java
@@ -1,162 +1,57 @@
 package rmk.virtusa.com.quizmaster;
 
 
-
-import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.content.Context;
-import android.content.Intent;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.core.Tag;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import rmk.virtusa.com.quizmaster.R;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import com.firebase.client.ValueEventListener;
 import java.util.Date;
-import java.util.TimeZone;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import android.util.Log;
 
-public class DashActivity extends AppActivity  {
 
 
-    private static final String TAG =DashActivity.class.getSimpleName();
-    TextView mTest;
-   TextView mStart;
-   TextView mTo;
-   TextView mEnd;
-   TextView sample;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Schedule extends AppCompatActivity{
+
+
+    private static final String TAG =Schedule.class.getSimpleName(); ;
+    Firebase ref1,ref2;
+    String egs,fgs;
    Button mButton;
-   String egs,fgs;
-   public Firebase href,gref;
-    static int flag=0;
+   TextView sample;
+   static int flag= 0;
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash);
-
-        mTest=findViewById(R.id.test);
-        mStart=findViewById(R.id.start);
-        mTo=findViewById(R.id.to);
-        mEnd=findViewById(R.id.end);
-        sample=findViewById(R.id.samp);
-        mButton=findViewById(R.id.button);
-
-        mButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    Intent myIntent = new Intent(DashActivity.this, MainActivity1.class);
-                    DashActivity.this.startActivity(myIntent);
-            }
-        });
-
-
-
-
-       /* mButton=(Button)findViewById(R.id.button);
-        mTest=(TextView)findViewById(R.id.test);
-        mStart=(TextView)findViewById(R.id.start);
-        mTo=(TextView)findViewById(R.id.to);
-        mEnd=(TextView)findViewById(R.id.end);*/
-
-
-        mTest.setText("MCQ TEST");
-        mTo.setText("To");
-        mButton.setText("Start Test");
-
-
+        setContentView(R.layout.schedule);
 
         Firebase.setAndroidContext(this);
-        href= new Firebase("https://quizmaster-89038.firebaseio.com/From");
-        href.addValueEventListener(new com.firebase.client.ValueEventListener() {
+        ref1= new Firebase("https://adminnapp.firebaseio.com/");
+
+        ref1.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
-                egs=dataSnapshot.getValue(String.class);
-                mStart.setText(egs);
-                
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
-            }
-        });
-        Firebase.setAndroidContext(this);
-        gref= new Firebase("https://quizmaster-89038.firebaseio.com/To");
-        gref.addValueEventListener(new com.firebase.client.ValueEventListener() {
-            @Override
-            public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
-                fgs=dataSnapshot.getValue(String.class);
-                Log.v(TAG, fgs);
-                mEnd.setText(fgs);
-                Date currentTime= new Date();
-                SimpleDateFormat sdf=new SimpleDateFormat("HH:mm");
-
-                String strDate= sdf.format(currentTime.getTime());
-
-
-                Date d3= null;
-                try {
-                    d3 = sdf.parse(strDate);
-
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-                Log.d(TAG, currentTime.toString());
-
-
-                Date d1= null;
-                try {
-                    d1 = sdf.parse(String.valueOf(egs));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                Date d2= null;
-                try {
-                    d2 = sdf.parse(String.valueOf(fgs));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-
-                try {
-                    if (d3.getTime() >= d1.getTime() && d3.getTime() <= d2.getTime() && flag==0) {
-                        
-                        flag=1;
-                        mButton.setClickable(true);
-                        sample.setText("enabled");
-                        Toast.makeText(getApplicationContext(), "Button Enabled", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, "onCreate: buttoncalled.");
-
-                    }
-                    else
-                    {
-                    mButton.setClickable(false);
-                        
-                    }
-                } catch (NullPointerException e){
-                    e.printStackTrace();
-                }
+                collectDetails((Map<String,Object>) dataSnapshot.getValue());
 
             }
 
@@ -166,82 +61,126 @@ public class DashActivity extends AppActivity  {
             }
         });
 
-
-
-        String st = egs;
-        String et = fgs;
+        // void collectDetails(Map<String,Object>,adminnapp) //
 
 
 
-//Comparison Code
+    }
 
-      /* Date currentTime= new Date();
-       SimpleDateFormat sdf=new SimpleDateFormat("HH:mm");
+    private void collectDetails(Map<String, Object> tests) {
 
-        String strDate= sdf.format(currentTime.getTime());
-
-
-            Date d3= new Date();
-        try {
-            d3 = sdf.parse(strDate);
+        LinearLayout ll= (LinearLayout) findViewById(R.id.info);
+        ll.setDividerPadding(8);
 
 
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        Log.d(TAG, currentTime.toString());
+        for (Map.Entry<String, Object> entry : tests.entrySet()){
 
 
-        Date d1= new Date();
-        try {
-            d1 = sdf.parse(String.valueOf(egs));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Date d2= new Date();
-        try {
-            d2 = sdf.parse(String.valueOf(fgs));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+            Map singleTest = (Map) entry.getValue();
+
+            TextView tv1 = new TextView(this);
+            tv1.setText("Test Name:  " + singleTest.get("mcqTest"));
+            TextView tv2 = new TextView(this);
+            tv2.setText("Start time:  "+ singleTest.get("fromTime"));
+            TextView tv3 = new TextView(this);
+            tv3.setText("End time: "+ singleTest.get("toTime"));
+           final Button b = new Button(this);
+          /*  b.setClickable(false);
+            b.setEnabled(false);
+            b.setText("Take Test");*/
 
 
 
-        if (d3.getTime() >= d1.getTime() && d3.getTime() <= d2.getTime()) {
-                mButton.setClickable(true);
-                sample.setText("button enabled");
-                Toast.makeText(this,"Button Enabled",Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onCreate: buttoncalled.");
+            ll.addView(tv1);
+            ll.addView(tv2);
+            ll.addView(tv3);
+         //   ll.addView(b);
 
+
+
+
+            Date currentTime = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+            String strDate = sdf.format(currentTime.getTime());
+
+
+            Date d3 = null;
+
+
+            try {
+                d3 = sdf.parse(strDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, currentTime.toString());
+            Date d1= null;
+
+            try {
+                d1 = sdf.parse(String.valueOf(singleTest.get("fromTime")));
+            } catch(ParseException e) {
+                e.printStackTrace();
+            }
+            Date d2=null;
+
+            try {
+                d2 = sdf.parse(String.valueOf(singleTest.get("toTime")));
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
 
 
-            Log.v(TAG, d3.toString()); */
+            try {
+                if (d3.getTime() >= d1.getTime() && d3.getTime() <= d2.getTime() ) {
 
 
 
 
-               /* mButton.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+                    b.setText("Take Test");
+                    ll.addView(b);
 
 
-                        Intent intent = new Intent(DashActivity.this, QuizActivity.class);
-                        startActivity(intent);
-                    }
-                });*/
-          /*  }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }*/
+                    //flag = 1;
+                    b.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            b.setClickable(false);
+                            b.setEnabled(false);
+                   b.setText("Take Test");
+                     b.setVisibility(View.GONE);
+
+                            Intent intent = new Intent(Schedule.this,Schedule.class);
+                            Schedule.this.startActivity(intent);
+                        }
+                    });
+                    //  sample.setText("enabled");
+                    // Toast.makeText(getApplicationContext(), "Button Enabled", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onCreate: buttoncalled.");
+
+                } else {
+
+                    //  Toast.makeText(getApplicationContext(), "Come back later", Toast.LENGTH_SHORT).show();
+
+                    b.setText("Come back later");
+                    b.setClickable(false);
+                    ll.addView(b);
 
 
 
+                }
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
 
 
 
+            String st = egs;
+            String et = fgs;
 
+
+        }
+
+
+    }
 }
 
-}
