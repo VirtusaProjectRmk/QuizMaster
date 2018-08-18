7c5b68933899788c8b33643fc0a03fec02bcdf08
Blessy Prarthana S <blessyprarthanasamuel@gmail.com>
2018-08-18 19:34:32 +0530

    Add files via upload

---
+++ app/src/main/java/rmk/virtusa/com/quizmaster/TestDetails.java
@@ -0,0 +1,29 @@
+package com.example.thanu.adminnapp;
+
+public class TestDetails {
+
+    String mcqTest;
+    String fromTime;
+    String toTime;
+
+
+    public TestDetails(String mcqTest, String fromTime, String toTime) {
+        this.mcqTest = mcqTest;
+        this.fromTime = fromTime;
+        this.toTime = toTime;
+    }
+
+
+
+  /*  public String getMcqtest() {
+        return mcqtest;
+    }
+
+    public String getFromtime() {
+        return fromtime;
+    }
+
+    public String getTotime() {
+        return totime;
+    }*/
+}
