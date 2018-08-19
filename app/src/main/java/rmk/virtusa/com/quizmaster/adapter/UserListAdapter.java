897413cc4d1edd241f771fd2236aefd45cbf20fd
Vignesh Baalaji <baalajivignesh@gmail.com>
2018-08-19 22:26:36 +0530

    Updated Leaderboard for Admin

--- app/src/main/java/rmk/virtusa/com/quizmaster/adapter/UserListAdapter.java
+++ app/src/main/java/rmk/virtusa/com/quizmaster/adapter/UserListAdapter.java
@@ -11,38 +11,45 @@ import android.widget.TextView;
 import java.util.List;
 
 import rmk.virtusa.com.quizmaster.R;
+import rmk.virtusa.com.quizmaster.handler.UserHandler;
 import rmk.virtusa.com.quizmaster.model.User;
 
-    public class UserListAdapter extends ArrayAdapter<User> {
+public class UserListAdapter extends ArrayAdapter<User> {
 
-        private Activity context;
-        private List<User> ulist;
+    private Activity context;
+    private List<User> ulist;
+    boolean admin = UserHandler.getInstance().getIsAdmin();
 
-        public UserListAdapter(Activity context, List<User> ulist)
-        {
-            super(context, R.layout.listlayout,ulist);
-            this.context = context;
-            this.ulist = ulist;
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater inflater = context.getLayoutInflater();
-            View listViewItem = inflater.inflate(R.layout.listlayout,null,true);
-
-            TextView empid = (TextView) listViewItem.findViewById(R.id.empid);
-            TextView name = (TextView) listViewItem.findViewById(R.id.name);
-            TextView branch = (TextView) listViewItem.findViewById(R.id.branch);
-            //TextView score = (TextView) listViewItem.findViewById(R.id.score);
-
-            User users = ulist.get(position);
-
-            empid.setText(users.getId());
-            name.setText(users.getName());
-            branch.setText(users.getBranch());
-            //score.setText(String.valueOf(users.getPointsTot()));
-
-            return  listViewItem;
-        }
+    public UserListAdapter(Activity context, List<User> ulist)
+    {
+        super(context, R.layout.listlayout,ulist);
+        this.context = context;
+        this.ulist = ulist;
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.listlayout,null,true);
+
+        TextView empid = (TextView) listViewItem.findViewById(R.id.empid);
+        TextView name = (TextView) listViewItem.findViewById(R.id.name);
+        TextView branch = (TextView) listViewItem.findViewById(R.id.branch);
+        TextView score = (TextView) listViewItem.findViewById(R.id.score);
+
+        User users = ulist.get(position);
+
+        empid.setText(users.getId());
+        name.setText(users.getName());
+        branch.setText(users.getBranch());
+
+        if(admin)
+            score.setText(String.valueOf(users.getPoints()));
+
+        else
+            score.setText("");
+
+        return  listViewItem;
+    }
+}
